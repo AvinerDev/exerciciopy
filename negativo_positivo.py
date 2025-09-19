a952d2c1ed6088bce1626efaa2c9e188bd379fc2
Aviner Oliveira <aviner6465@gmail.com>
2025-09-19 13:04:00 -0300

    negativo e positivo

---
+++ negativo_positivo.py
@@ -0,0 +1,7 @@
+numero = float(input("digite um numero: "))
+if numero > 0:
+    print("o numero e POSITIVO.")
+elif numero < 0:
+    print("o numero e NEGATIVO.")
+else:
+    print("o numero e ZERO")        
